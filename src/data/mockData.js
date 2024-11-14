21dc19881384d4bd24357085e6de32f6770b1df7
SilviaKocianova <sissi.kocianova@gmail.com>
2024-11-14 22:18:34 +0100

    first commit

---
+++ src/data/mockData.js
@@ -0,0 +1,14 @@
+export const MOCK_SHOPPING_LIST = {
+    id: '1',
+    name: 'Weekly Groceries',
+    items: [
+      { id: 'item1', name: 'Apples', resolved: false },
+      { id: 'item2', name: 'Bread', resolved: true },
+      { id: 'item3', name: 'Milk', resolved: false },
+    ],
+    owner: { id: 'owner1', name: 'John Doe' },
+    members: [
+      { id: 'member1', name: 'Jane Smith' },
+      { id: 'member2', name: 'Sam Green' },
+    ],
+  };
