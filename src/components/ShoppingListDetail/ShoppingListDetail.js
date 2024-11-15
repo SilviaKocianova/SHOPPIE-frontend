35326b3f6c21f2a76e35c4cca5156fcfef174c2e
SilviaKocianova <sissi.kocianova@gmail.com>
2024-11-15 12:44:50 +0100

    unresolved

--- src/components/ShoppingListDetail/ShoppingListDetail.js
+++ src/components/ShoppingListDetail/ShoppingListDetail.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './ShoppingListDetail.css';
+import unresolvedSvg from '../img/unresolved.svg';
+
 
 const ShoppingListDetail = ({ list, user, onAddItem, onRemoveItem, onMarkAsResolved, onEditName }) => {
   const [newItem, setNewItem] = useState('');
@@ -27,27 +29,43 @@ const ShoppingListDetail = ({ list, user, onAddItem, onRemoveItem, onMarkAsResol
 
   return (
     <div className="shopping-list-detail">
+      <div className="shopping-list-detail-box">
+      <div className="title-and-edit">
       <h2 className="list-title">{list.name}</h2>
-      {user && <p className="list-owner">Owner: {user.name}</p>}
 
-      {/* Render the edit button only if the user is the owner */}
-      {user?.isOwner && (
+            {/* Render the edit button only if the user is the owner */}
+            {user?.isOwner && (
         <button className="edit-button" onClick={handleEdit}>
-          Edit Name
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5" width="20" height="20">
+  <path d="m2.695 14.762-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" />
+</svg>
+
         </button>
       )}
 
-      <ul className="shopping-list">
-        {list.items.map(item => (
-          <li key={item.id} className={`shopping-list-item ${item.resolved ? 'resolved' : ''}`}>
-            {item.name}
-            <button onClick={() => onMarkAsResolved(item.id)}>
-              {item.resolved ? 'Unresolve' : 'Resolve'}
-            </button>
-            <button onClick={() => onRemoveItem(item.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      </div>
+      {user && <p className="list-owner">Owner: {user.name}</p>}
+      
+
+
+      
+
+<ul className="shopping-list">
+  {list.items.map(item => (
+    <li key={item.id} className={`shopping-list-item ${item.resolved ? 'resolved' : ''}`}>
+      {item.name}
+      <button class="resolve-unresolve" onClick={() => onMarkAsResolved(item.id)}>
+        {item.resolved ? 'Unresolve' : (
+          <img src={unresolvedSvg} alt="Unresolved" width="20" height="20" />
+        )}
+      </button>
+      <button onClick={() => onRemoveItem(item.id)}>Remove</button>
+    </li>
+  ))}
+</ul>
+
+
+
 
       <div className="add-item-section">
         <input
@@ -57,6 +75,7 @@ const ShoppingListDetail = ({ list, user, onAddItem, onRemoveItem, onMarkAsResol
           placeholder="Enter new item"
         />
         <button onClick={handleAddItem}>Add Item</button>
+      </div>
       </div>
     </div>
   );
